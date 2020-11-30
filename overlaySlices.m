38b0d750d80f23a0b0a72124f3b8abf08dd86870
Jochen Hammes <hammesj@me.com>
2020-11-30 12:40:33 +0100

    write DICOM Include title for lower part

--- overlaySlices.m
+++ overlaySlices.m
@@ -1,9 +1,8 @@
-function [OutputRGB] = overlaySlices(pathToInputNii, pathToDicom)
+function [OutputRGB] = overlaySlices(pathToInputNiiBPND, pathToInputNiiZmap, pathToDicom)
 %% Environment
 
-%Test
-
-pathToInputNii = 'inputImages/BET_zDev_automatedCGN_SRTM2_BPnd_image.nii';
+%pathToInputNiiBPND = 'inputImages/BET_automatedCGN_SRTM2_BPnd_image.nii';
+%pathToInputNiiZmap = 'inputImages/BET_zDev_automatedCGN_SRTM2_BPnd_image.nii';
 pathMNI_T1 = 'TemplateImages/ch2_79x75x78.nii';
 pathToDicom = '/DATA/hammesj/PI2620_KinMod/Gripp/';
 dcms = dir([pathToDicom '**']);
@@ -21,24 +20,28 @@ pathToDicom = [dcms(1).folder filesep dcms(1).name]
 
 MipThickness = 5;
 
-%Load  Nii-File
-InputNii = load_nii(pathToInputNii);
+%Load  Nii-Files
+InputNiiZMap = load_nii(pathToInputNiiZmap);
+InputNiiBPND = load_nii(pathToInputNiiBPND);
 MNI_T1 = load_nii(pathMNI_T1);
 
-maxVoxelValue = max(InputNii.img(:));
+maxVoxelValue = max(InputNiiZMap.img(:));
 upperThreshholdSlices = maxVoxelValue/2;
 lowerThreshholdSlices = maxVoxelValue/8;
+upperThreshholdSlicesBPND = 1;
+lowerThreshholdSlicesBPND = 0.1;
 
-MatrixSize = size(InputNii.img);
+MatrixSize = size(InputNiiZMap.img);
 EvenMatrixSize = MatrixSize+mod(MatrixSize,2);
 
 
-ySize=500;
+ySize=970;
 xSize=455;
 
 yRow(1) = 10; % Offset of Top Part of Output RGB
 yRow(2)  = 20; % Offset of Middle Part of Output RGB
-yRow(3)  = 100;% Offset of Lower Part of Output RGB <-- Slices go here
+yRow(3)  = 100;% Offset of First Image-Part  of Output RGB <-- Slices go here
+yRow(4)  = 570;% Offset of Second Image-Part of Output RGB <-- Second Slices go here
 
 xOffsetColorBar = 410;
 
@@ -60,7 +63,7 @@ myMap(1:2,:)=0;
 %myMap((end-2):end,:)=1;
 
 
-%% Creation and placement of RGB-Slices
+%% Creation and placement of RGB-Slices (first part, ZMap)
 
 for i=1:15
     %imshow(NewRGB)
@@ -80,7 +83,7 @@ for i=1:15
         verticalOffset = 300;
         horizontalOffset = 10+100*((i-12)-1);
     end
-    CurrentSliceMono = monoProjectionPlaneFromDevMap(InputNii,10,'axial',5*(i-1)+1,5*i);
+    CurrentSliceMono = monoProjectionPlaneFromDevMap(InputNiiZMap,10,'axial',5*(i-1)+1,5*i);
     CurrentSliceRGB = RGBFromMonoPlane(CurrentSliceMono, myMap, upperThreshholdSlices);
 
     %Place T1-MRI    
@@ -95,12 +98,53 @@ for i=1:15
 end
 
 
+
+
+%% Creation and placement of RGB-Slices (second part, BPND)
+
+for i=1:15
+    %imshow(NewRGB)
+    %pause(0.5)
+    
+    verticalOffset = 0;
+    horizontalOffset = 10+100*(i-1);
+    if (i>4 & i<9)
+        verticalOffset = 100;
+        horizontalOffset = 10+100*((i-4)-1);
+    end
+    if (i>8 & i<13)
+        verticalOffset = 200;
+        horizontalOffset = 10+100*((i-8)-1);
+    end
+    if i>12
+        verticalOffset = 300;
+        horizontalOffset = 10+100*((i-12)-1);
+    end
+    CurrentSliceMono = monoProjectionPlaneFromDevMap(InputNiiBPND,10,'axial',5*(i-1)+1,5*i);
+    CurrentSliceRGB = RGBFromMonoPlane(CurrentSliceMono, myMap, upperThreshholdSlicesBPND);
+
+    %Place T1-MRI    
+    CurrentSliceMNI_T1 = monoProjectionPlaneFromDevMap(MNI_T1,10,'axial',5*i-1,5*i);
+    NewRGB = placeMonoOnRGB(NewRGB, fliplr(imrotate(CurrentSliceMNI_T1, 90)), horizontalOffset,yRow(4)+verticalOffset); 
+    
+    %Place Z-Map with transparency Map
+    transparencyMap = fliplr(imrotate((CurrentSliceMono > lowerThreshholdSlicesBPND),90));
+    NewRGB = placeRGBImage(NewRGB, fliplr(imrotate(CurrentSliceRGB, 90)), horizontalOffset,yRow(4)+verticalOffset, transparencyMap); 
+    
+
+end
+
+
+
+
+
 %% Color Bars
 
 ColorBar = RGBFromMonoPlane(repmat((256:-2:1)',1,10), myMap);
 %NewRGB = placeRGBImage(NewRGB, ColorBar, xOffsetColorBar,yRow(1));
 %NewRGB = placeRGBImage(NewRGB, ColorBar, xOffsetColorBar,yRow(2));
 NewRGB = placeRGBImage(NewRGB, ColorBar, xOffsetColorBar,yRow(3));
+NewRGB = placeRGBImage(NewRGB, ColorBar, xOffsetColorBar,yRow(4));
 
 
 %% Resize image
@@ -128,15 +172,21 @@ textDelim = '''';
 systemCommandToDrawText = ['convert -pointsize 50 -fill white -draw "text 20,60 ' ...
     textDelim 'PI2620 Tau-PET Analysis' textDelim ...
     ' " slices.png newslices.png'];
-system(systemCommandToDrawText)
+system(systemCommandToDrawText);
 
 systemCommandToDrawText = ['convert -pointsize 35 -fill white -draw "text 20,100 ' ...
     textDelim 'Z-transformed deviations of non displaceable binding from controls' textDelim ...
     ' " newslices.png newslices.png'];
-system(systemCommandToDrawText)
+system(systemCommandToDrawText);
 
 
-%Draw annotations to LUT
+systemCommandToDrawText = ['convert -pointsize 35 -fill white -draw "text 20,' num2str((yRow(4) -20) * outputScalingFactor) ' ' ...
+    textDelim 'PI2620 non displaceable binding ' textDelim ...
+    ' " newslices.png newslices.png'];
+system(systemCommandToDrawText);
+
+
+%Draw annotations to upper LUT
 systemCommandToDrawText = ['convert -pointsize 35 -fill white -draw "text ' ...
     num2str(xOffsetColorBar * outputScalingFactor) ...
     ',' num2str(yRow(3) * outputScalingFactor - 20) ' ' ...
@@ -161,6 +211,31 @@ systemCommandToDrawText = ['convert -pointsize 35 -fill white -draw "text ' ...
 system(systemCommandToDrawText)
 
 
+%Draw annotations to lower LUT
+systemCommandToDrawText = ['convert -pointsize 35 -fill white -draw "text ' ...
+    num2str(xOffsetColorBar * outputScalingFactor) ...
+    ',' num2str(yRow(4) * outputScalingFactor - 20) ' ' ...
+    textDelim 'BP' textDelim ...
+    ' " newslices.png newslices.png'];
+system(systemCommandToDrawText)
+
+
+systemCommandToDrawText = ['convert -pointsize 35 -fill white -draw "text ' ...
+    num2str(xOffsetColorBar * outputScalingFactor + 40) ...
+    ',' num2str(yRow(4) * outputScalingFactor + 40) ' ' ...
+    textDelim num2str(upperThreshholdSlicesBPND, '%2.1f') textDelim ...
+    ' " newslices.png newslices.png'];
+system(systemCommandToDrawText)
+
+
+systemCommandToDrawText = ['convert -pointsize 35 -fill white -draw "text ' ...
+    num2str(xOffsetColorBar * outputScalingFactor + 40) ...
+    ',' num2str(yRow(4) * outputScalingFactor + 350) ' ' ...
+    textDelim num2str(lowerThreshholdSlicesBPND, '%2.1f') textDelim ...
+    ' " newslices.png newslices.png'];
+system(systemCommandToDrawText)
+
+
 
 
 
@@ -175,8 +250,13 @@ systemCommandToDrawText = ['convert -pointsize 35 -fill white -draw "text 20,200
     ' " newslices.png newslices.png'];
 system(systemCommandToDrawText)
 
+%% Rename File
 
+movefile('newslices.png', 'TIP2_Output.png');
 
+%% Save DICOM File
+
+dicomwrite(imread('TIP2_Output.png'), 'TIP2_Output.dcm');
 
 %% return result bitmap
 OutputRGB = NewRGB;
